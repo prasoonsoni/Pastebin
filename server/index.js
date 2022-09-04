a6bec350653d4e168178ef51bc53457f69b619db
prasoonsoni <prasoonsoni03@gmail.com>
2022-09-04 20:06:01 +0530

    feat: :card_file_box: Database Connectivity

--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
-
+const connect = require('./database/connection')
 app.use(cors())
 app.use(express.json())
+connect()
 
 app.get('/', (req, res) => {
     res.send('success')
