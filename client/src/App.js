1c075827bbade4cdb588a51ffbf850410f0d4695
prasoonsoni <prasoonsoni03@gmail.com>
2022-09-05 20:34:22 +0530

    feat: 🔧 Create Functionality Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Home from "./pages/Home";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 function App() {
   return (
-    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/edit/:id" element={<Home />} />
+      </Routes>
       <Home />
-    </>
+    </Router>
   );
 }
 
--- client/src/components/Home/CreateNew.js
+++ client/src/components/Home/CreateNew.js
@@ -1,10 +1,41 @@
-import React from 'react'
-import { Button } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { Button, useToast } from '@chakra-ui/react'
 import { AddIcon } from '@chakra-ui/icons'
+import { useNavigate } from 'react-router-dom'
+const BASE_URL = process.env.REACT_APP_BASE_URL
 
 const CreateNew = () => {
+    const toast = useToast()
+    const navigate = useNavigate()
+    const [loading, setLoading] = useState(false)
+    const handleOnCreate = async () => {
+        console.log(BASE_URL)
+        setLoading(true)
+        const response = await fetch(`${BASE_URL}/api/create`, {
+            method: 'POST'
+        })
+        const data = await response.json()
+        if (data.success) {
+            toast({
+                title: data.message,
+                status: "success",
+                duration: 2000,
+                isClosable: true,
+            })
+            navigate(`/edit/${data.content._id}`)
+        } else {
+            toast({
+                title: data.message,
+                status: "error",
+                duration: 2000,
+                isClosable: true,
+            })
+        }
+        setLoading(false)
+        console.log(data)
+    }
     return (
-        <Button leftIcon={<AddIcon />} colorScheme="linkedin" w="300px">Create</Button>
+        <Button leftIcon={<AddIcon />} isLoading={loading} loadingText="Creating..." colorScheme="linkedin" w="300px" onClick={handleOnCreate}>Create</Button>
     )
 }
 
