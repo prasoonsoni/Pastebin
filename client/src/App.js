596e09ea8a8130ff21d314cdb96f7f6fd0e81d27
prasoonsoni <prasoonsoni03@gmail.com>
2022-09-05 17:59:09 +0530

    style: 💄 Basic UI Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Nunito",
+    body: "Montserrat Alternates"
+  }
+})
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
