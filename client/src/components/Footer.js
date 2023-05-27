163dc1bb83f5759e3c446274dcb9d2b6b991f02d
Prasoon Soni <prasoonsoni03@gmail.com>
2023-05-27 22:53:22 +0530

    Updated Portfolio URL

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <Box pos="fixed" bottom="0" h="40px" w="100%" bg="">
       <Center h="100%">
         <HStack>
-          <Text fontSize="md">Made with ❤️ by <Link href='https://prasoon.codes' isExternal color="blue.500">Prasoon Soni</Link></Text> 
+          <Text fontSize="md">Made with ❤️ by <Link href='https://prasoonsoni.com' isExternal color="blue.500">Prasoon Soni</Link></Text> 
           <Text> | </Text>
           <Link href='https://github.com/prasoonsoni/Pastebin' isExternal color="blue.500">Github</Link>
         </HStack>
